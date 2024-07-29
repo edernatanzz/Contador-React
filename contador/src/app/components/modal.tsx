d51509fc4dd1d8951ce9eae907eb959d074624bb
EderNatan.dev <edernatan124@gmail.com>
2024-07-29 11:05:41 -0300

    modal

--- contador/src/app/components/modal.tsx
+++ contador/src/app/components/modal.tsx
@@ -1,11 +1,38 @@
+'use client';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh; /* Ajusta a altura para cobrir toda a tela */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Conteudo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 250px;
+  background: rgba(255, 255, 255, 0.5);
+`;
+const Text = styled.div`
+
+
+`
 
 export function Modal() {
-    return (
-        <>
-           
-        </>
-    );
+  return (
+    <Container>
+      <Conteudo>
+        <Text>
+            <h1>Contagem regressiva</h1>
+            dias, horas , minutos, segundos
+        </Text>
+      </Conteudo>
+    </Container>
+  );
 }
 
 export default Modal;
